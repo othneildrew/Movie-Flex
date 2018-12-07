e8c4dd88e616501170e061359f35c99ec99ea8c1
Othneil I Drew <codeguydrew@gmail.com>
2018-12-07 11:11:06 -0600

    Inital commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$(function(){
+
+
+  init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function init() {
+    // Initialize sticky kit
+    //$('#topnav').stick_in_parent();
+
+
+    console.log('init function called');
+
+
+  }
+
+
+  function config() {
+    const apiKey = '0fd55b792d815c99792d8ba07533e9f6';
+    const baseURL = 'https://api.themoviedb.org/3/';
+    let baseImageURL = 'https://image.tmbdb.org/';
+    configData,
+    baseImageURL;
+
+
+
+  }
+
+  function getMovies(type) {
+    $.get()
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
