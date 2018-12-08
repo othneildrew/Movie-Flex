45932792549dc526649118828c98323b0e66bacb
Othneil I Drew <codeguydrew@gmail.com>
2018-12-08 14:17:51 -0600

    Changed design and color scheme, refactored code

--- js/main.js
+++ js/main.js
@@ -2,15 +2,6 @@ $(function() {
 
   init();
 
-  hidePageOverlay();
-
-
-
-
-
-
-
-
 
 
 
@@ -19,76 +10,72 @@ $(function() {
     $('.titles-slider').slick({
       lazyLoad: 'ondemand',
       infinite: false,
-      speed: 800,
+      speed: 600,
       slidesToShow: 6,
-      slidesToScroll: 1,
+      slidesToScroll: 6,
       responsive: [{
           breakpoint: 1200,
           settings: {
             slidesToShow: 6,
-            slidesToScroll: 1,
+            slidesToScroll: 1
           }
         },
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 5,
-            slidesToScroll: 1,
+            slidesToScroll: 5
           }
         },
         {
           breakpoint: 992,
           settings: {
             slidesToShow: 4,
-            slidesToScroll: 1,
+            slidesToScroll: 4
           }
         },
         {
           breakpoint: 768,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 1
+            slidesToScroll: 3
           }
         },
         {
           breakpoint: 576,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 1
+            slidesToScroll: 2
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     });
 
 
+    fetchMovies('#now-playing', 'movie/now_playing?page=1&language=en-US&api_key=');
 
-    console.log('init function called');
+    fetchMovies('#popular', 'movie/popular?page=1&language=en-US&api_key=');
 
+    fetchMovies('#upcoming', 'movie/upcoming?page=1&language=en-US&api_key=');
 
+    fetchMovies('#trending', 'trending/movie/day?api_key=');
+
+    fetchMovies('#top-rated', 'movie/top_rated?page=1&language=en-US&api_key=');
+
+    hidePageOverlay();
   }
+
+
+
+
+
+
 
   function hidePageOverlay() {
     $('.page-overlay').fadeOut(300).css({'visibility': 'hidden'});
   }
 
 
-  function config() {
-    const apiKey = '0fd55b792d815c99792d8ba07533e9f6';
-    const baseURL = 'https://api.themoviedb.org/3/';
-    let baseImageURL = 'https://image.tmbdb.org/';
-    configData,
-    baseImageURL;
-
-
-
-  }
-
-  function getMovies(type) {
-    $.get()
-  }
 
 
 
@@ -97,9 +84,28 @@ $(function() {
 
 
 
-  $('.title').hover(function() {
-    $(this).children().children('.title-category').toggle();
-  });
+  function fetchMovies(sliderID, requestURL) {
+    let settings = {
+      async: true,
+      crossDomain: true,
+      url: BASE_URL + requestURL + API_KEY,
+      method: 'GET',
+      headers: {},
+      data: '{}'
+    }
+
+    $.ajax(settings).done(function(response) {
+      $.each(response.results, function(key, value) {
+        //slideIndex++;
+        $(sliderID + '-slider').slick('slickAdd', '<div class="title mb-4"><a id="'+ value.id +'" href="javascript:void(0)"><div class="title-img-container"><div class="title-rating"><i class="fas fa-star"></i> <span>'+ value.vote_average +'</span></div><img data-lazy="https://image.tmdb.org/t/p/w342/'+ value.poster_path +'" alt=""></div><p class="title-name text-truncate">'+ value.original_title +'</p></a></div>');
+      });
+    });
+  } // END fetchMovies
+
+
+
+
+
 
 
 
